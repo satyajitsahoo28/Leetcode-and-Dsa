18a33795d4d3bc0a7e33bbea992de6b78a9749f8
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-02-26 10:22:18 +0530

    Update rotate-array.java

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,4 +1,20 @@
 class Solution {
+    //it is not handle the big size array
+    public void rotate(int[] nums, int k) {
+
+        int count=0;
+        int minusOne;
+
+        while(count<k){
+            minusOne=nums[nums.length-1];
+            for(int i=nums.length-1;i>0;i--){
+                nums[i]=nums[i-1];
+            }
+            nums[0]=minusOne;
+            count++;
+        }
+        
+    }
 
     public void reverse(int[] nums,int si,int ei){
         while(si<=ei){
