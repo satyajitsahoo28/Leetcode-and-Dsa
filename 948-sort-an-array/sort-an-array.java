f7c7680c5b01748961ec8eb102830100e36549d7
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-02-26 09:45:02 +0530

    Time: 26 ms (67.94%) | Memory: 56.3 MB (40.43%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void mergeSort(int[] nums,int si,int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int mid=si+(ei-si)/2;
+        mergeSort(nums,si,mid);
+        mergeSort(nums,mid+1,ei);
+        merge(nums,si,mid,ei);
+    }
+
+    public void merge(int[] nums,int si,int mid,int ei){
+        int temp[]=new int[ei-si+1];
+        int k=0;
+        int i=si;
+        int j=mid+1;
+
+        while(i<=mid && j<=ei){
+            if(nums[i]<nums[j]){
+                temp[k++]=nums[i++];
+            }
+
+            else{
+                temp[k++]=nums[j++];
+            }
+        }
+
+        while(i<=mid){
+            temp[k++]=nums[i++];
+        }
+
+        while(j<=ei){
+            temp[k++]=nums[j++];
+        }
+
+        for(k=0,i=si;k<temp.length;k++,i++){
+            nums[i]=temp[k];
+        }
+
+
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+
+        int[] ans=new int[nums.length];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=nums[i];
+        }
+
+        return ans;
+        
+    }
+}
