708d4bbac2cc8185995e5828b6c01f4b52003aea
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-03-13 10:31:12 +0530

    Update missing-number.java

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,19 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // int temp[]=new int[nums.length+1];
+        //method-1
+        int temp[]=new int[nums.length+1];
 
-        // for(int i=0;i<nums.length;i++){
-        //     temp[nums[i]]++;
-        // }
+        for(int i=0;i<nums.length;i++){
+             temp[nums[i]]++;
+         }
         
-        // int num=0;
-        // for(int i=0;i<temp.length;i++){
-        //     if(temp[i]==0){
-        //         num=i;
-        //     }
-        // }
+         int num=0;
+         for(int i=0;i<temp.length;i++){
+             if(temp[i]==0){
+                 num=i;
+             }
+         }
 
-        // return num;
+        return num;
+
+        //method-2
         int n=nums.length;
         int sum1=(n*(n+1))/2;
         int sum2=0;
