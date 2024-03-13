65cc1519a7a6db361ad4102c25ec67602e33da57
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-03-13 20:17:57 +0530

    Time: 3 ms (27.02%) | Memory: 45.7 MB (90.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,47 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int temp[]=new int[nums.length];
-       int si=0;
-       int ei=nums.length-1;
-       int i=0;
+    //    int temp[]=new int[nums.length];
+    //    int si=0;
+    //    int ei=nums.length-1;
+    //    int i=0;
 
-       while(si<=ei && i<nums.length){
-        if(nums[i]==0){
-            temp[ei]=nums[i];
-            i++;
-            ei--;
+    //    while(si<=ei && i<nums.length){
+    //     if(nums[i]==0){
+    //         temp[ei]=nums[i];
+    //         i++;
+    //         ei--;
+    //     }
+    //     else{
+    //         temp[si]=nums[i];
+    //         i++;
+    //         si++;
+    //     }
+
+    //    }
+    //    for(int j=0;j<nums.length;j++){
+    //     nums[j]=temp[j];
+    //    }
+
+    int i=0;
+    int j=0;
+
+    while(i<nums.length && j<nums.length){
+        if(nums[j]==0){
+            j++;
         }
+
+        else if(nums[j]!=0 && nums[i]==0){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+        }
+
         else{
-            temp[si]=nums[i];
             i++;
-            si++;
+            j++;
         }
-
-       }
-       for(int j=0;j<nums.length;j++){
-        nums[j]=temp[j];
-       }
+    }
 
     }
 }
