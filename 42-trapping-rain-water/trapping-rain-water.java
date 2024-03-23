742c2a5349643ca4b8d0182e64669ecb80bfd716
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-03-23 11:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (90.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,56 @@
 class Solution {
     public int trap(int[] height) {
-        int prefix[]=new int[height.length];
-        int suffix[]=new int[height.length];
+        // int prefix[]=new int[height.length];
+        // int suffix[]=new int[height.length];
 
-        int max=height[0];
-        for(int i=0;i<prefix.length;i++){
-            prefix[i]=Math.max(max,height[i]);
-            max=prefix[i];
-        }
+        // int max=height[0];
+        // for(int i=0;i<prefix.length;i++){
+        //     prefix[i]=Math.max(max,height[i]);
+        //     max=prefix[i];
+        // }
 
-        max=height[height.length-1];
-        for(int i=suffix.length-1;i>=0;i--){
-            suffix[i]=Math.max(height[i],max);
-            max=suffix[i];
-        }
-        int trap=0;
-        for(int i=0;i<height.length;i++){
-              trap =trap+ Math.min(prefix[i], suffix[i]) - height[i];
+        // max=height[height.length-1];
+        // for(int i=suffix.length-1;i>=0;i--){
+        //     suffix[i]=Math.max(height[i],max);
+        //     max=suffix[i];
+        // }
+        // int trap=0;
+        // for(int i=0;i<height.length;i++){
+        //       trap =trap+ Math.min(prefix[i], suffix[i]) - height[i];
             
+        // }
+
+        // return trap;
+
+        int left=0;
+        int right=height.length-1;
+        int leftMax=0;
+        int rightMax=0;
+        int res=0;
+
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>leftMax){
+                    leftMax=height[left];
+                }
+                else{
+                    res+=leftMax-height[left];
+                }
+                left++;
+            }
+
+            else{
+                if(height[right]>rightMax){
+                    rightMax=height[right];
+                }
+                else{
+                    res+=rightMax-height[right];
+                }
+                right--;
+            }
         }
 
-        return trap;
+        return res;
         
     }
 }
