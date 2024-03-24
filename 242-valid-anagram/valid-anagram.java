d192712172a75fb9bf2b7fa54bba2c7628597975
Satyajit0003 <149392619+Satyajit0003@users.noreply.github.com>
2024-03-24 17:30:33 +0530

    Time: 5 ms (53.09%) | Memory: 42.5 MB (96.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,13 +3,27 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char str1[]=s.toCharArray();
-        Arrays.sort(str1);
-        char str2[]=t.toCharArray();
-        Arrays.sort(str2);
+        // char str1[]=s.toCharArray();
+        // Arrays.sort(str1);
+        // char str2[]=t.toCharArray();
+        // Arrays.sort(str2);
 
-        for(int i=0;i<str1.length;i++){
-            if(str1[i]!=str2[i]){
+        // for(int i=0;i<str1.length;i++){
+        //     if(str1[i]!=str2[i]){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0){
                 return false;
             }
         }
