035fa28086072a7459052e234ebf630ff8f8b0d8
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-04-17 12:05:44 +0530

    Time: 4 ms (45.13%) | Memory: 44.1 MB (34.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -20,14 +20,18 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int ans[]=nextGreater(nums2);
         int result[]=new int[nums1.length];
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums2.length;i++){
+            map.put(nums2[i],ans[i]);
+        }
 
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    result[i]=ans[j];
-                }
+            if(map.containsKey(nums1[i])){
+                result[i]=map.get(nums1[i]);
             }
         }
+        
         return result;
         
     }
