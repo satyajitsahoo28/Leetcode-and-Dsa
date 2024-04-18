f3306e639a805051d694cd7f13e6c73df9d63cb5
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-04-18 16:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (73.45%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,38 +1,71 @@
 class Solution {
+    // public int countStudents(int[] students, int[] sandwiches) {
+    //     Queue<Integer> q=new LinkedList<>();
+    //     Stack<Integer> s=new Stack<>();
+
+    //     for(int i=0;i<students.length;i++){
+    //         q.add(students[i]);
+    //     }
+
+    //     for(int i=sandwiches.length-1;i>=0;i--){
+    //         s.push(sandwiches[i]);
+    //     }
+    //     int count=0;
+    //     while(!s.isEmpty()){
+    //         count=0;
+    //         if(s.peek()==q.peek()){
+    //             s.pop();
+    //             q.remove();
+    //         }
+    //         else{
+    //             while(s.peek()!=q.peek() && count<s.size()){
+    //                 int val=q.remove();
+    //                 q.add(val);
+    //                 count++;
+    //             }
+    //             if(s.peek()==q.peek()){
+    //                 s.pop();
+    //                 q.remove();
+    //             }
+    //             else{
+    //               return s.size();  
+    //             }
+
+    //         }
+    //     }
+    //     return s.size();
+    // }
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q=new LinkedList<>();
-        Stack<Integer> s=new Stack<>();
-
+        int n=sandwiches.length;
+        int s_zero=0;
+        int s_one=0;
         for(int i=0;i<students.length;i++){
-            q.add(students[i]);
-        }
-
-        for(int i=sandwiches.length-1;i>=0;i--){
-            s.push(sandwiches[i]);
-        }
-        int count=0;
-        while(!s.isEmpty()){
-            count=0;
-            if(s.peek()==q.peek()){
-                s.pop();
-                q.remove();
+            if(students[i]==0){
+                s_zero++;
             }
             else{
-                while(s.peek()!=q.peek() && count<s.size()){
-                    int val=q.remove();
-                    q.add(val);
-                    count++;
-                }
-                if(s.peek()==q.peek()){
-                    s.pop();
-                    q.remove();
-                }
-                else{
-                  return s.size();  
-                }
-
+                s_one++;
             }
         }
-        return s.size();
+
+        for(int i=0;i<sandwiches.length;i++){
+        if(sandwiches[i]==0){
+            if(sandwiches[i]==0 && s_zero>0){
+                s_zero--;
+            }
+            else{
+                return n-i;
+            }
+        }
+        if(sandwiches[i]==1){
+            if(sandwiches[i]==1 && s_one>0){
+                s_one--;
+            }
+            else{
+                return n-i;
+            }
+        }
+        }
+        return 0;
     }
 }
