a8b6a1357e6710acfea1475363b170328c9498fc
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-05-01 10:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (70.60%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,27 +1,37 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int idx=-1;
-        String ans="";
-        for(int i=0;i<word.length();i++){
-            if(word.charAt(i)==ch){
-               idx=i;
-               break;
-            }
-        }
-        String reverse="";
-        String leftWord="";
-        if(idx!=-1){
-            for(int i=idx;i>=0;i--){
-                reverse+=word.charAt(i);
-            }
+        // int idx=-1;
+        // String ans="";
+        // for(int i=0;i<word.length();i++){
+        //     if(word.charAt(i)==ch){
+        //        idx=i;
+        //        break;
+        //     }
+        // }
+        // String reverse="";
+        // String leftWord="";
+        // if(idx!=-1){
+        //     for(int i=idx;i>=0;i--){
+        //         reverse+=word.charAt(i);
+        //     }
 
-            for(int i=idx+1;i<word.length();i++){
-                leftWord+=word.charAt(i);
-            }
-        }
-        else{
+        //     for(int i=idx+1;i<word.length();i++){
+        //         leftWord+=word.charAt(i);
+        //     }
+        // }
+        // else{
+        //     return word;
+        // }
+        // return reverse+leftWord;
+        int firstOccurence = word.indexOf(ch);
+        if (firstOccurence == -1) {
             return word;
         }
-        return reverse+leftWord;
+        StringBuilder sb = new StringBuilder(word.substring(0, firstOccurence + 1)).reverse();
+        if (firstOccurence < word.length()) {
+            sb.append(word.substring(firstOccurence + 1));
+        }
+        return sb.toString();
+
     }
 }
