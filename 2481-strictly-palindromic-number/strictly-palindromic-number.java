5b8ee73063cea8ced66ffa215e165cc35a36a35d
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-05-27 23:45:42 +0530

    Time: 1 ms (13.90%) | Memory: 40.3 MB (49.87%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,5 +1,5 @@
 class Solution {
-     boolean decimalToBase(int n,int b) 
+     String decimalToBase(int n,int b) 
     { 
         int remainder, quotient = n; 
         String binaryNum = ""; 
@@ -8,20 +8,24 @@ class Solution {
             binaryNum = Integer.toString(remainder) + binaryNum; 
             quotient = quotient / b; 
         } 
+        return binaryNum; 
+    } 
+    boolean checkPalindrome(String s){
         int si=0;
-        int ei=binaryNum.length()-1;
-        while(si<ei){
-            if(binaryNum.charAt(si)!=binaryNum.charAt(ei)){
+        int ei=s.length()-1;
+        while(si<=ei){
+            if(s.charAt(si)!=s.charAt(ei)){
                 return false;
             }
             si++;
             ei--;
         }
-        return true; 
-    } 
+        return true;
+    }
     public boolean isStrictlyPalindromic(int n) {
         for(int i=2;i<=n-2;i++){
-            if(!decimalToBase(n,i)){
+            String s=decimalToBase(n,i);
+            if(!checkPalindrome(s)){
                 return false;
             }
         }
