a02070f2d7af7680d19bf2d6ef22d2e1bc18a7b2
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-09-12 20:02:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,34 +1,50 @@
 class Solution {
-    public int houseRobberRec(int ind, int[] nums){
-        if(ind<0) return 0;
-        if(ind==0) return nums[0];
 
-        int robHouse = nums[ind] + houseRobberRec(ind-2, nums);
-        int not_robHouse = 0 + houseRobberRec(ind-1, nums);
+    public int houseRobberRec(int ind, int[] nums) {
+        if (ind < 0) return 0;
+        if (ind == 0) return nums[0];
+
+        int robHouse = nums[ind] + houseRobberRec(ind - 2, nums);
+        int not_robHouse = 0 + houseRobberRec(ind - 1, nums);
 
         return Math.max(robHouse, not_robHouse);
     }
+
     public int houseRobberMemo(int ind, int[] nums, int[] dp) {
-         if (ind == 0) return nums[0];
+        if (ind == 0) return nums[0];
         if (ind < 0) return 0;
-       
-        
+
         if (dp[ind] != -1) return dp[ind];
-        
+
         int robHouse = nums[ind] + houseRobberMemo(ind - 2, nums, dp);
         int notRobHouse = houseRobberMemo(ind - 1, nums, dp);
-        
+
         return dp[ind] = Math.max(robHouse, notRobHouse);
     }
-    
+
     public int rob(int[] nums) {
         if (nums.length == 0) return 0;
-        
+        //Recursion
+        //houseRobberRec(nums.length - 1, nums);
+
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        
-         //houseRobberRec(nums.length - 1, nums);  
-        
-        return houseRobberMemo(nums.length - 1, nums, dp);  
+        //memoziztion
+        // Arrays.fill(dp, -1);
+        // return houseRobberMemo(nums.length - 1, nums, dp);
+
+        //tabulation
+        Arrays.fill(dp, 0);
+        dp[0] = nums[0];
+        for (int ind = 1; ind < nums.length; ind++) {
+            int robHouse = nums[ind];
+            if (ind  > 1) {
+                robHouse = dp[ind - 2];
+            }
+            int notRobHouse = dp[ind - 1];
+
+            dp[ind] = Math.max(robHouse, notRobHouse);
+        }
+
+        return dp[dp.length - 1];
     }
 }
