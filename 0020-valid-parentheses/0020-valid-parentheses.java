be66f48c1e8118ed2c218d840a53056c536441e9
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-09-27 19:52:27 +0530

    Time: 1 ms (98.56%), Space: 41.4 MB (70.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            } else{
+                if(st.isEmpty()){
+                    return false;
+                } else{
+                    if(st.peek()=='(' && ch==')' ||
+                       st.peek()=='{' && ch=='}' ||
+                       st.peek()=='[' && ch==']'){
+                        st.pop();
+                       } else{
+                        return false;
+                       }
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
