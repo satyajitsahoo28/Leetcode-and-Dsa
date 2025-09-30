fff49998d7554b929125d9c5d8847e4695641a52
Satyajit Sahoo <149392619+satyajitsahoo28@users.noreply.github.com>
2025-09-30 12:18:12 +0530

    Time: 194 ms (24.95%), Space: 42.1 MB (25.74%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,51 +1,41 @@
 class Solution {
-    // Direction vectors: up, right, down, left
-    int[] dr = {-1, 0, 1, 0};
-    int[] dc = {0, 1, 0, -1};
-
-    public boolean solve(char[][] board, int r, int c, int idx, String word) {
-        // Base case: all characters matched
-        if (idx == word.length()) {
+    int dr[]={-1,0,1,0};
+    int dc[]={0,1,0,-1};
+    public boolean solve(char[][] board,String word,boolean[][] vis,int r,int c,int idx){
+        if(idx==word.length()){
             return true;
         }
 
-        // Out of bounds or character mismatch
-        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != word.charAt(idx)) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || vis[r][c] || board[r][c] != word.charAt(idx)) {
             return false;
         }
 
-        // Temporarily mark this cell as visited
-        char temp = board[r][c];
-        board[r][c] = '#';
-
-        // Explore all 4 directions
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if (solve(board, nr, nc, idx + 1, word)) {
+        vis[r][c]=true;
+        
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(solve(board,word,vis,nr,nc,idx+1)){
                 return true;
             }
         }
-
-        // Backtrack: restore the cell
-        board[r][c] = temp;
+        vis[r][c]=false;
         return false;
     }
-
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        // Try to start DFS from every cell
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (solve(board, i, j, 0, word)) {
+        int n=board.length;
+        int m=board[0].length;
+        boolean[][] vis=new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(solve(board,word,vis,i,j,0)){
                         return true;
                     }
                 }
             }
         }
+        
         return false;
     }
 }
