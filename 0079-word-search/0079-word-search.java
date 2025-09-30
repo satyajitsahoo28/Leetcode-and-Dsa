f87087721c32c279d5eefc9b57771269128e315b
Satyajit Sahoo <149392619+satyajitsahoo28@users.noreply.github.com>
2025-09-30 12:05:33 +0530

    Time: 174 ms (30.76%), Space: 41.6 MB (78.63%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,49 +1,48 @@
 class Solution {
-    public boolean f(int row, int col, int ind, char[][] board, boolean[][] vis, String word, StringBuilder ans, int[] dr, int[] dc) {
-        if (ind == word.length()) {
-            if (ans.toString().equals(word)) {
-                return true;
-            } else {
-                return false;
-            }
+    // Direction vectors: up, right, down, left
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
+
+    public boolean solve(char[][] board, int r, int c, int idx, String word) {
+        // Base case: all characters matched
+        if (idx == word.length()) {
+            return true;
         }
 
+        // Out of bounds or character mismatch
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != word.charAt(idx)) {
+            return false;
+        }
+
+        // Temporarily mark this cell as visited
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        // Explore all 4 directions
         for (int i = 0; i < 4; i++) {
-            int newRow = row + dr[i];
-            int newCol = col + dc[i];
-
-            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-                char ch = word.charAt(ind);
-                if (ch == board[newRow][newCol] && !vis[newRow][newCol]) {
-                    vis[newRow][newCol] = true; // mark as visited
-                    ans.append(ch);
-                    if (f(newRow, newCol, ind + 1, board, vis, word, ans, dr, dc)) {
-                        return true;
-                    }
-                    ans.deleteCharAt(ans.length() - 1); // backtrack
-                    vis[newRow][newCol] = false; // unmark as visited
-                }
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (solve(board, nr, nc, idx + 1, word)) {
+                return true;
             }
         }
+
+        // Backtrack: restore the cell
+        board[r][c] = temp;
         return false;
     }
 
     public boolean exist(char[][] board, String word) {
-        boolean[][] vis = new boolean[board.length][board[0].length];
-        int[] dr = { -1, 0, 1, 0 };
-        int[] dc = { 0, 1, 0, -1 };
-        StringBuilder ans = new StringBuilder();
+        int m = board.length;
+        int n = board[0].length;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        // Try to start DFS from every cell
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
-                    vis[i][j] = true; // mark starting point as visited
-                    ans.append(board[i][j]);
-                    if (f(i, j, 1, board, vis, word, ans, dr, dc)) {
+                    if (solve(board, i, j, 0, word)) {
                         return true;
                     }
-                    ans.deleteCharAt(ans.length() - 1); // backtrack
-                    vis[i][j] = false; // unmark the starting point
                 }
             }
         }
