23c577fd4ec9d3eaa077449f6c2a3bf5a559ae8a
Satyajit Sahoo <149392619+satyajitsahoo28@users.noreply.github.com>
2025-11-10 20:17:05 +0530

    Time: 2 ms (47.59%), Space: 47.1 MB (84.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,38 +1,57 @@
 class Solution {
-    public void dfs(int node,boolean[] vis, List<List<Integer>> adj){
-        vis[node]=true;
-        for(int it:adj.get(node)){
-            if(!vis[it]){
-                dfs(it,vis,adj);
-            }
+    int par[];
+    int rank[];
+
+    public int find(int u){
+        if(u==par[u]){
+            return u;
+        }
+        return par[u]=find(par[u]);
+    }
+
+    public void union(int u,int v){
+        int par_u=find(u);
+        int par_v=find(v);
+
+        if(rank[par_u]>rank[par_v]){
+            par[par_v]=par[par_u];
+        }else if(rank[par_u]<rank[par_v]){
+            par[par_u]=par[par_v];
+        }else{
+            rank[par_u]+=1;
+            par[par_v]=par[par_u];
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adj=new ArrayList<>();
         int n=isConnected.length;
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+        par=new int[n];
+        rank=new int[n];
+        for(int i=1;i<n;i++){
+            par[i]=i;
+            rank[i]=1;
         }
-
-        boolean[] vis=new boolean[n];
-        int cnt=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+                if(isConnected[i][j]==1 && i!=j){
+                    int u=i;
+                    int v=j;
+
+                    int par_u=find(u);
+                    int par_v=find(v);
+
+                    if(par_u!=par_v){
+                        union(par_u,par_v);
+                    }
                 }
             }
         }
-
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
+            if(i==par[i]){
                 cnt++;
             }
         }
-        
         return cnt;
     }
 }
